e2787c1380098814655e7433f734938807a69ba9
Gravit'a <12893402+gravit0@users.noreply.github.com>
2023-10-13 15:21:43 +0700

    Fix crash on new vs compiler

--- GravitGuard/GravitGuardAPI.h
+++ GravitGuard/GravitGuardAPI.h
@@ -4,15 +4,15 @@
 class GravitGuardAPI
 {
 public:
-	void (*test)();
-	void (*init)();
-	std::string(*toUtf8)(std::wstring& wstr);
-	void (*log)(unsigned char type, std::string_view module_name, std::string_view data);
-	void (*jvmCreatedCallback)(void* jvm_ptr, void* jvm_env);
+	void (* volatile test)();
+	void (* volatile init)();
+	std::string(* volatile toUtf8)(std::wstring& wstr);
+	void (* volatile log)(unsigned char type, std::string_view module_name, std::string_view data);
+	void (* volatile jvmCreatedCallback)(void* jvm_ptr, void* jvm_env);
 	// License data
-	void (*printLicense)();
-	std::pair<char*, size_t>(*generate_license)(std::string username, const char* pubkey_data, unsigned int pubkey_size, char* privatekey);
-	void (*licenseData)(std::string key);
+	void (* volatile printLicense)();
+	std::pair<char*, size_t>(* volatile generate_license)(std::string username, const char* pubkey_data, unsigned int pubkey_size, char* privatekey);
+	void (* volatile licenseData)(std::string key);
 };
 
 extern "C"  GravitGuardAPI GUARD_EXPORT getGuardAPI();
